12452c391c8f6a412dd1ca5ed9e8a498a0e0c9f2
Hassan ElZanfaly <hassanelzanfaly1234@gmail.com>
2024-05-21 16:15:17 +0300

    remove fake store api and make my own products data located at data.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const products = []
-const apiURL = 'https://fakestoreapi.com/products'
+import {categories} from './data.js'
 
 const sectionTemplate = document.querySelector('#section-template')
 const cardTemplate = document.querySelector('#card-template')
@@ -8,19 +7,23 @@ const main = document.querySelector('main')
 const searchBar = document.querySelector('input[type="search"]')
 const form = document.querySelector('form')
 
-async function loadProductsData(section, categoryName){
-    const res = await fetch(`${apiURL}/category/${categoryName}`)
-    const data = await res.json()
+const products = []
 
+for (let i = 0; i < categories.length; i++){
+    const section = sectionTemplate.content.cloneNode(true).children[0]
+    section.querySelector('h2').textContent = `Best Sellers in ${categories[i].name}`
+
+    const data = categories[i].products
+    
     for (let i = 0; i < data.length; i++){
         const card = cardTemplate.content.cloneNode(true).children[0]
         const productImg = card.querySelector('.product-img')
         const productName = card.querySelector('.product-name')
         const productPrice = card.querySelector('.product-price')
 
-        productImg.src = data[i].image
+        productImg.src = data[i].img
         productName.textContent = data[i].title
-        productPrice.textContent = `${Math.round(Number(data[i].price) * 46.64)}`
+        productPrice.textContent = data[i].price
 
         section.querySelector('.cards-container').appendChild(card)
         products.push(data[i])
@@ -28,24 +31,6 @@ async function loadProductsData(section, categoryName){
     main.appendChild(section)
 }
 
-fetch(`${apiURL}/categories`)
-.then(res => res.json())
-.then(categories => {
-    for (let i = 0; i < categories.length; i++){
-        const section = sectionTemplate.content.cloneNode(true).children[0]
-        section.querySelector('h2').textContent = `Best Sellers in ${categories[i]}`
-        loadProductsData(section, categories[i])
-    }
-})
-
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    // let value = searchBar.value.toLowerCase()
-    // for (let i = 0; i < products.length; i++){
-    //     const isVisible = products[i].title.toLowerCase().includes(value) || products[i].description.toLowerCase().includes(value)
-    //     if (isVisible){
-    //         console.log(products[i])
-    //     }
-    // }
-    // searchBar.value = ''
 })
